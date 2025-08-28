b6978fcdc14ecbc48b8eaa1084c85d3b0b83ee49
innefear1 <innerfear1@outlook.com>
2025-08-28 08:00:01 +0800

    add SD framework

---
+++ Stabl_diffusion/Laten_SD.py
@@ -0,0 +1,20 @@
+# code by inner
+from diffusers import StableDiffusionPipeline
+
+class model_setting:
+    '''模型设置'''
+    def __init__(self) -> None:
+        pass
+
+class img2img:
+    '''图生图'''
+    def __init__(self) -> None:
+        pass
+
+class text2img:
+    '''文生图'''
+    def __init__(self) -> None:
+        pass
+
+if __name__ == '__main__':
+    pass
